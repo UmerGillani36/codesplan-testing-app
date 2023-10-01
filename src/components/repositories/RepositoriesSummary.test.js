eac88d53a541aa8aeca1a9fd996655222012d4fd
UmerGillani36 <syedomershahgillani@gmail.com>
2023-10-01 14:23:04 +0500

    displays info about the repository

--- src/components/repositories/RepositoriesSummary.test.js
+++ src/components/repositories/RepositoriesSummary.test.js
@@ -1,7 +1,7 @@
 import {screen , render} from '@testing-library/react';
 import RepositoriesSummary from './RepositoriesSummary';
 
-test('displays primary language of the repository',()=>{
+test('displays info about the repository',()=>{
     const repository = {
         language:'Javascript',
         stargazers_count:5,
@@ -9,8 +9,10 @@ test('displays primary language of the repository',()=>{
         forks:30
     }
     render(<RepositoriesSummary repository={repository}/>);
+    for(let key in repository){
+        const value = repository[key];
+        const element = screen.getByText(new RegExp(value));
+        expect(element).toBeInTheDocument();
+    }
 
-    const language = screen.getByText('Javascript');
-
-    expect(language).toBeInTheDocument();
 })
