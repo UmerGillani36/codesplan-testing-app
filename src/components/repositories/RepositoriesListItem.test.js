917381d59a3bb0aac83278a3e7a1e50985c30663
umergillani36 <syedomershahgillani@gmail.com>
2023-10-29 02:06:25 -0700

    solveung act warning with findByRole

---
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import RepositoriesListItem from '../repositories/RepositoriesListItem';
+import { MemoryRouter } from "react-router-dom";
+
+const RenderComponent = () => {
+  const repository = {
+    full_name: "facebook/react",
+    language: "Javascript",
+    description: "A js library",
+    owner: "facebook",
+    name: "react",
+    html_url: "https://github.com/facebook/react",
+  };
+  render(
+    <MemoryRouter>
+        <RepositoriesListItem  repository={repository}/>
+    </MemoryRouter>
+  )
+};
+test("shows a link to the github homepage for this repository", () => {
+  RenderComponent();
+  screen.findByRole('img',{name:'Javascript'});
+});
