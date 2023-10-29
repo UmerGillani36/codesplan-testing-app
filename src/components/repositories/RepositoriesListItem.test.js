ea5b1c3070b41f7c53d993d3e962bbebfabc3ec7
umergillani36 <syedomershahgillani@gmail.com>
2023-10-29 06:09:16 -0700

    adding test for the repo icon

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
-import RepositoriesListItem from '../repositories/RepositoriesListItem';
+import RepositoriesListItem from "../repositories/RepositoriesListItem";
 import { MemoryRouter } from "react-router-dom";
+import { async } from "validate.js";
 
 // jest.mock('../tree/FileIcon.js',()=>{
 //   return ()=>{
@@ -19,18 +20,26 @@ const RenderComponent = () => {
   };
   render(
     <MemoryRouter>
-        <RepositoriesListItem  repository={repository}/>
+      <RepositoriesListItem repository={repository} />
     </MemoryRouter>
-  )
-  return {repository};
+  );
+  return { repository };
 };
 test("shows a link to the github homepage for this repository", async () => {
-  const {repository} = RenderComponent();
-  await  screen.findByRole('img',{name:'Javascript'});
+  const { repository } = RenderComponent();
+  await screen.findByRole("img", { name: "Javascript" });
 
-  const link = screen.getByRole('link',{
-    name: /github repository/i
+  const link = screen.getByRole("link", {
+    name: /github repository/i,
   });
 
-  expect(link).toHaveAttribute('href', repository.html_url)
+  expect(link).toHaveAttribute("href", repository.html_url);
+});
+
+test("shows a fileIcon with the appropriate icon", async () => {
+  RenderComponent();
+
+  const icon = await screen.findByRole("img", { name: "Javascript" });
+
+  expect(icon).toHaveClass("js-icon");
 });
