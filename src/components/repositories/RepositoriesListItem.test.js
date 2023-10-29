ea61b76f614f21a0bf052d59b167eb61a61356c0
umergillani36 <syedomershahgillani@gmail.com>
2023-10-29 06:27:06 -0700

    adding new test for link

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -14,7 +14,9 @@ const RenderComponent = () => {
     full_name: "facebook/react",
     language: "Javascript",
     description: "A js library",
-    owner: "facebook",
+    owner: {
+      login: "facebook",
+    },
     name: "react",
     html_url: "https://github.com/facebook/react",
   };
@@ -40,6 +42,13 @@ test("shows a fileIcon with the appropriate icon", async () => {
   RenderComponent();
 
   const icon = await screen.findByRole("img", { name: "Javascript" });
-
   expect(icon).toHaveClass("js-icon");
+});
+test("shows a link to the code editor page", async () => {
+  const { repository } = RenderComponent();
+
+  const link = await screen.findByRole("link", {
+    name: new RegExp(repository.owner.login),
+  });
+  expect(link).toHaveAttribute("href", `/repositories/${repository.full_name}`);
 });
